f57e1ef457f27f3c1d739b6626733cb7d5fb15d4
Alexander Urizar <urizaraj@gmail.com>
2018-02-26 17:14:13 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -25,7 +25,7 @@ class Application
         @@cart << search_term
         resp.write("added #{search_term}")
       else
-        resp.write('Error')
+        resp.write("We don't have that item")
       end
     else
       resp.write "Path Not Found"
